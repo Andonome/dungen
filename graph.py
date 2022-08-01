ecbcf088d4fbbee6ca3fb0c53951249e556886d3
Malin Freeborn <malinfreeborn@posteo.net>
2022-08-02 01:54:09 +0200

    add multiple connections between rooms

--- graph.py
+++ graph.py
@@ -1,14 +1,16 @@
 def graph(dungeon):
-    for x in range(2,len(dungeon)):
-        second = dungeon[x]["connections"]
-        print(
-            '[ ' + str(second)
-            + ': ' + dungeon[second]['name']
-            + ' ]' + ' --> ' + '[ '
-            + str(x) + ': '
-            + dungeon[x]['name'] + ' ]'
-        )
-        #print('[ ' + str(x) + ': ' + dungeon[x]['name']
-        #+ ' --> ' + str(dungeon[x]['name']))
-
-
+    for x in range(2, len(dungeon)):
+        for second in dungeon[x]["connections"]:
+            print(
+                "[ "
+                + str(second)
+                + ": "
+                + dungeon[second]["name"]
+                + " ]"
+                + " --> "
+                + "[ "
+                + str(x)
+                + ": "
+                + dungeon[x]["name"]
+                + " ]"
+            )
--- phase1.py
+++ phase1.py
@@ -2,29 +2,6 @@ import random
 import pprint
 from areas import *
 
-### Plan ###
-#  The final result might look something like this:
-#
-#     "entrance": {
-#         "name": "entrance",
-#         "leadsto": [2],
-#         "description": """
-# This is where people come in.
-#
-# Hopefully there will be at least one entrance.
-# """,
-#     },
-#     "alcove": {
-#         "name": "alcove",
-#         "leadsto": [3,4]
-#         "description": """
-# A room without a view
-# """,
-# --------------------
-# So we get a series of rooms, and their connections.
-# Initial values we take from the areas, then add the key 'leadsto'.
-#
-
 
 def show(x):
     pprint.pprint(x)
@@ -37,7 +14,7 @@ def makeDunList(setting):
     # With 'entrance' as area 1, 'count' starts at 2.
     # The count only increases when we have a non-repeating room, to avoid getting rooms 1,2,4,6.
     count = 2
-    while count < 19:
+    while count < 15:
         areaChoice = random.choice(list(areas.keys()))
         if setting in areas[areaChoice]["ecosystems"] and areaChoice != oldChoice:
             # The original piece of code was this:
@@ -52,11 +29,12 @@ def makeDunList(setting):
 
 
 def joinDun(dungeon):
+    joins = 2
     for x in range(2, len(dungeon) + 1):
-        if random.randint(1, 3) == 2 and x >= 3:
-            dungeon[x]["connections"] = random.randint(2, x - 1)
+        dungeon[x]["connections"] = []
+        while random.randint(1, joins) == 1 and x >= 3:
+            dungeon[x]["connections"].append(random.randint(2, x - 1))
+            joins += 1
         else:
-            dungeon[x]["connections"] = x - 1
+            dungeon[x]["connections"].append(x - 1)
     return dungeon
-
-
