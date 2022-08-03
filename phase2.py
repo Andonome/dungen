fbdf2d3bf7c477fbd26c3bfa2148ae8c056b8e94
Malin Freeborn <malinfreeborn@posteo.net>
2022-08-03 09:58:38 +0200

    start elf wishlist

--- phase2.py
+++ phase2.py
@@ -1,8 +1,37 @@
 from constructions import *
 
-def civilize(dungeon):
-    for x in range(1, len(dungeon)): 
-        if dungeon[x]["name"] == "chasm":
-            dungeon[x]["constructions"] = "bridge"
-    return(dungeon)
+# This list shows what places gain which features.
+# A chasm would require a bridge over it to be liveable.
+
+global roomTransformations
+roomTransformations = [
+    ['chasm', 'bridge',],
+    ['lava', 'bridge'],
+    ['entrance', 'trap'],
+]
+
+# This ordered wishlist makes the elves make one alcove into a kitchen,
+# then one cavern into a library.
+
+elfWishList = [
+    ["alcove", "kitchen", 1],
+    ["cavern", "library", 1],
+    ["alcove", "forge", 1],
+    ["cavern", "musicHall",1],
+    ["alcove", "bedroom", 20],
+]
+
+def fixRooms(dungeon):
+    for x in range(len(dungeon)-1,1,-1):
+        for pair in range(len(roomTransformations)):
+            if dungeon[x]['name'] == roomTransformations[pair][0]:
+                dungeon[x]['constructions'] = roomTransformations[pair][1]
+
+# The elves want to build a home, so they start by converting some alcove
+
+def makeHome(dungeon):
+    for x in range(len(dungeon),2,-1):
+        if dungeon[x]['name'] == 'cavern':
+            dungeon[x]['constructions'] = roomTransformations[pair][1]
+
 
