bd40c655ca0d5594a9c5fd8099fe147b883f95da
Malin Freeborn <malinfreeborn@posteo.net>
2022-08-05 19:32:21 +0200

    add various goblins during invasion

--- phase3.py
+++ phase3.py
@@ -22,9 +22,31 @@ def breakRooms(dungeon):
 
 
 def makeGoblins(dungeon):
-    for x in range(len(dungeon) - 1, 1, -1):
-        if "bedroom" in dungeon[x]["features"]:
-            dungeon[x]["creatures"].append(str(random.randint(3, 9)) + " goblins")
+    # First let's see how big the dungeon is - larger dungeons
+    # should house most goblins, and having a fungal garden
+    # should also increase their number.
+    goblinNo = int(len(dungeon) / 3)
+    for x in range(1, len(dungeon) - 1):
+        gobEnc = random.randint(1, goblinNo)
+        if "fungus" in dungeon[x]["features"]:
+            goblinNo += 1
+            dungeon[x]["creatures"].append("ooze")
+        elif "lake" in dungeon[x]["features"]:
+            dungeon[x]["creatures"].append("ooze")
+        elif "river" in dungeon[x]["features"]:
+            pass
+        elif gobEnc > 5:
+            dungeon[x]["creatures"].append(str(random.randint(1, 4)) + " ogres")
+        elif goblinNo > 4:
+            dungeon[x]["creatures"].append(
+                str(random.randint(1, goblinNo)) + " hobgoblins"
+            )
+        elif goblinNo > 2:
+            dungeon[x]["creatures"].append(
+                str(random.randint(1, goblinNo * 2)) + " goblins"
+            )
+        else:
+            pass
 
 
 def rampage(dungeon):
