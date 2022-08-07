fefa953fb54f18f54d300373cafdb7c98bede39f
Malin Freeborn <malinfreeborn@posteo.net>
2022-08-07 05:32:35 +0200

    embiggen traps

--- traps.py
+++ traps.py
@@ -1,13 +1,18 @@
 traps = {
     "rocksFall": {
         "name": "falling rocks",
-        "races": [ "dwarvse", "goblins",]
+        "races": [ "dwarves", "goblins",],
         "description": "Rocks fall. Everyone dies"
    },
     "falseFloor": {
         "name": "falling rocks",
-        "races": [ "dwarvse", "goblins",]
-        "description": "Rocks fall. Everyone dies"
+        "races": [ "dwarvse", "goblins",],
+        "description": "The rickety wooden floor here give way to a ten foot deep hole, full of nasty, pointy things",
+   },
+    "gasLeak": {
+        "name": "gas leak",
+        "races": [ "dwarves", "goblins",],
+        "description": "The natural gas in this area will grant 5 Fatigue Points to anyone hanging around, every round",
    },
 
 }
