5e510e9e253006423f8e92fb54c5dace3a58baca
Malin Freeborn <malinfreeborn@posteo.net>
2022-08-10 16:00:19 +0200

    make splits bigger

--- graph.py
+++ graph.py
@@ -10,6 +10,7 @@ def graph(setting,dungeon):
     dunMap.attr("node", shape='egg',overlap='false')
     dunMap.node('areaType',setting,shape='tripleoctagon',fontsize='60')
     for x in range(len(dungeon)):
+        fontsize = 10
         roomShape = 'ellipse'
         contents = str(x) + ": \n" + "\n ".join(dungeon[x]["features"])
         if len(dungeon[x]["creatures"]) > 0:
@@ -20,11 +21,17 @@ def graph(setting,dungeon):
             roomShape = "star"
         elif "entrance" in dungeon[x]["type"]:
             roomShape = "doublecircle"
+            fontsize = 15
         elif "split" in dungeon[x]["type"]:
             roomShape = "hexagon"
+            fontsize = 15
         elif "dead end" in dungeon[x]["type"]:
             roomShape = "invhouse"
-        dunMap.node(str(x), contents,shape=roomShape)
+        dunMap.node(
+        str(x),
+        contents,shape=roomShape,
+        fontsize=str(fontsize),
+        )
         for connection in dungeon[x]["connections"]:
             dunMap.edge(str(x), str(connection))
     dunMap.render("Test Map.gv")
